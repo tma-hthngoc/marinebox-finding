5c84f3a8e41692f368c77129eaa1d894ef8d36e0
hthngoc <hthngoc@tma.com.vn>
2015-01-02 14:55:22 +0700

    1. Rewrite scanning port 3001 to prevent missing IP + loop timeout, 2.Update the starting point. 3.Move setting to app.config instead of hard-code

--- MarineBoxFinding/Program.cs
+++ MarineBoxFinding/Program.cs
@@ -5,8 +5,11 @@ using System.Collections.Generic;
 using System;
 using Microsoft.Win32;
 using System.IO;
-using System.Net.NetworkInformation;
 using System.Net.Sockets;
+using System.Web;
+using System.Net;
+using System.Text;
+using System.Configuration;
 
 namespace MarineBoxFinding
 {
@@ -14,53 +17,14 @@ namespace MarineBoxFinding
     {
         public static List<Ping> pingers = new List<Ping>();
         public static int instances = 0;
-
-        public static object @lock = new object();
-
-        public static int result = 0;
         public static int timeOut = 250;
-
         public static int ttl = 5;
-        public static string totallines = "";
-
-        public static void Ping_completed(object s, PingCompletedEventArgs e)
-        {
-            lock (@lock)
-            {
-                instances -= 1;
-            }
-
-            if (e.Reply.Status == IPStatus.Success)
-            {
-                TcpClient tcpClient = new TcpClient();
-                try
-                {
-                    tcpClient.Connect(e.Reply.Address.ToString(), 3001);
-                    Console.WriteLine(string.Concat("Active IP of MarineBox (3001): ", e.Reply.Address.ToString()));
-                    totallines += string.Concat("Active IP of MarineBox (3001): ", e.Reply.Address.ToString() + Environment.NewLine);
-                    result += 1;
-                    //in case of generate bookmark, change javascript file here
-                    //Change here...
-                }
-                catch (Exception)
-                {
-
-                    Console.WriteLine("Port " + 3001 + " Closed");
-                }
-            }
-            else
-            {
-                //Console.WriteLine(String.Concat("Non-active IP: ", e.Reply.Address.ToString()))
-            }
-        }
-
 
         public static void CreatePingers(int cnt)
         {
             for (int i = 1; i <= cnt; i++)
             {
                 Ping p = new Ping();
-                p.PingCompleted += Ping_completed;
                 pingers.Add(p);
             }
         }
@@ -69,39 +33,45 @@ namespace MarineBoxFinding
         {
             foreach (Ping p in pingers)
             {
-                p.PingCompleted -= Ping_completed;
                 p.Dispose();
             }
-
             pingers.Clear();
-
         }
     }
 
     class Program
     {
-
         public static void Main()
         {
-            File.Delete("C:\\ResultOfMarineBoxFinding.txt");
-            String IPNetwork = "";
+            Configuration config = ConfigurationManager.OpenExeConfiguration(
+                System.Reflection.Assembly.GetExecutingAssembly().Location);
+            int myPort = Convert.ToInt32(config.AppSettings.Settings["myPort"].Value.ToString());
+            string startingPage = config.AppSettings.Settings["startingPage"].Value;
+            string fn = config.AppSettings.Settings["totalCheck"].Value;
+            string serverRQA = config.AppSettings.Settings["serverRQA"].Value;
+            List<string> activeIP = new List<string>();
+            String log = "1) Scanning Network ...";
+            Console.WriteLine(log);
+            File.Delete(serverRQA);
+            File.Delete(fn);
+
             foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
             {
                 Console.WriteLine(ni.Name);
                 if (ni.OperationalStatus != OperationalStatus.Up) continue;
 
-                IPNetwork += "MAC: " + ni.GetPhysicalAddress() + Environment.NewLine;
-                IPNetwork += "Gateways:" + Environment.NewLine;
+                log += "MAC: " + ni.GetPhysicalAddress() + Environment.NewLine;
+                log += "Gateways:" + Environment.NewLine;
 
                 Console.WriteLine("Operational? {0}", ni.OperationalStatus == OperationalStatus.Up);
                 Console.WriteLine("MAC: {0}", ni.GetPhysicalAddress());
                 Console.WriteLine("Gateways:");
                 foreach (GatewayIPAddressInformation gipi in ni.GetIPProperties().GatewayAddresses)
                 {
-                    IPNetwork += gipi.Address + Environment.NewLine;
+                    log += gipi.Address + Environment.NewLine;
                     Console.WriteLine("\t{0}", gipi.Address);
                 }
-                IPNetwork += "IP Addresses: ";
+                log += "IP Addresses: ";
                 Console.WriteLine("IP Addresses:");
                 String IPv4 = "";
                 foreach (UnicastIPAddressInformation uipi in ni.GetIPProperties().UnicastAddresses)
@@ -110,21 +80,16 @@ namespace MarineBoxFinding
                     {
                         if (uipi.Address.ToString() == "127.0.0.1") continue;
                         IPv4 = uipi.Address.ToString();
-                        IPNetwork += uipi.Address.ToString() + " Subnet Mask: " + uipi.IPv4Mask.ToString() + Environment.NewLine;
+                        log += uipi.Address.ToString() + " Subnet Mask: " + uipi.IPv4Mask.ToString() + Environment.NewLine;
                         Console.WriteLine("\t{0} / {1}", uipi.Address, uipi.IPv4Mask);
                     }
                 }
-
-
                 if (IPv4 == "" || IPv4 == "127.0.0.1") continue;
-
-
 
                 //ping entire network of NICs
                 string baseIP = IPv4.Substring(0, IPv4.LastIndexOf(".") + 1);
 
                 Console.WriteLine("Pinging 255 destinations of D-class in {0}*", baseIP);
-                PingNetwork.totallines = "";
 
                 //number of IP in subnet
                 PingNetwork.CreatePingers(255);
@@ -133,50 +98,149 @@ namespace MarineBoxFinding
                 System.Text.ASCIIEncoding enc = new System.Text.ASCIIEncoding();
                 byte[] data = enc.GetBytes("abababababababababababababababab");
 
-                //SpinWait wait = new SpinWait();
-                //Number of start IP address
                 int cnt = 1;
+                int index = 1;
 
-                Stopwatch watch = Stopwatch.StartNew();
-
+                // hthngoc - remove async running due to missing Active IP - to check RQA server.
+                // must run slowly in sequence.
                 foreach (Ping p in PingNetwork.pingers)
                 {
-                    lock (PingNetwork.@lock)
+                    PingReply reply= p.Send(string.Concat(baseIP, cnt.ToString()), PingNetwork.timeOut);
+                    if (reply.Status == IPStatus.Success)
                     {
-                        PingNetwork.instances += 1;
+                        Console.WriteLine(string.Concat("Active IP: ", reply.Address.ToString()));
+                        activeIP.Add(reply.Address.ToString());
+                        FindRQAServer(index, reply.Address.ToString(), myPort, serverRQA);
+                        index += 1;
+                        log += Environment.NewLine + reply.Address.ToString();
                     }
-
-                    p.SendAsync(string.Concat(baseIP, cnt.ToString()), PingNetwork.timeOut, data, po);
                     cnt += 1;
                 }
+            }            
 
-                while (PingNetwork.instances > 0)
-                {
-                    Thread.SpinWait(50);
-                }
+            Console.WriteLine("\n2)Updating the Starting point...\n");
+            string updatedResult = UpdateStartingPage(startingPage, serverRQA);
 
-                watch.Stop();
+            log += updatedResult;
+            File.AppendAllText(fn, log);
 
-                PingNetwork.DestroyPingers();
-
-                Console.WriteLine("Finished in {0}. Found {1} active IP-addresses.", watch.Elapsed.ToString(), PingNetwork.result);
-                PingNetwork.totallines += "Finished in " + watch.Elapsed.ToString() + ". Found " + PingNetwork.result + " active IP-addresses." + Environment.NewLine;
-
-                File.AppendAllText("C:\\ResultOfMarineBoxFinding.txt", PingNetwork.totallines);
-            }
-            //write to log file            
-            File.AppendAllText("C:\\ResultOfMarineBoxFinding.txt", IPNetwork);
             Console.WriteLine("=========");
             Console.WriteLine("=========");
             Console.WriteLine("=========");
             Console.WriteLine("=========");
             Console.WriteLine("=========");
-            Console.WriteLine("Done, please Press Any Key to quit, please get the file in C:\\ResultOfPing.txt  for Wallem support");
+            Console.WriteLine("Done, please Press Any Key to quit. \nPlease get the file in \"" + fn + "\" for Wallem support.");
             Console.WriteLine("=========");
             Console.WriteLine("=========");
             Console.WriteLine("=========");
             Console.WriteLine("=========");
             Console.ReadKey();
         }
+
+        /// <summary>
+        /// hthngoc - Separate with Scan IP func due to 
+        ///     + its async process.        
+        ///     + skip invalid pointed.
+        /// </summary>
+        /// <param name="ipList"></param>
+        public static void FindRQAServer(int i, string ip, int myPort, string serverRQAFile)
+        {
+            String toWrite = "";
+                string url = string.Format("http://{0}:{1}/", ip, myPort);
+                try
+                {
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                    request.Method = "GET";
+                    request.KeepAlive = true;
+                    request.UserAgent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0; .NET CLR 1.1.4322)";
+                    request.ContentType = "text/xml";
+                    request.Credentials = System.Net.CredentialCache.DefaultNetworkCredentials;
+                    request.Timeout = 1000;
+                    // execute the request
+                    using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                    {
+                        if (response.Headers["set-cookie"].Contains("RQA"))
+                        {
+                            Console.WriteLine(string.Format("{0}. Found RQA Server at: {1}", i.ToString(), url));
+                            toWrite += ip + ";";
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(string.Format("{0}. Skipped {1}", i.ToString(), url));
+                }
+                File.AppendAllText(serverRQAFile, toWrite);
+        }
+        /// <summary>
+        /// hthngoc - update IP in source Starting point.
+        /// Use the smallest IP if have many RQA servers.
+        /// </summary>
+        /// <param name="startingPage"></param>
+        /// <param name="serverRQA"></param>
+        public static string UpdateStartingPage(string startingPage, string serverRQA)
+        {
+            string rs =  Environment.NewLine + "2) List of RQA servers:" + Environment.NewLine;
+            Console.WriteLine(rs);
+            if (File.Exists(startingPage))
+            {
+                string html = File.ReadAllText(startingPage);
+                string servers = File.ReadAllText(serverRQA);
+                string[] serverList = servers.Split(';');
+                IPAddress[] list = new IPAddress[serverList.Length-1];
+                
+                for (int i = 0; i < serverList.Length-1; i++)
+                {
+                    if (serverList[i] != string.Empty)
+                    {
+                        list[i] = IPAddress.Parse(serverList[i]);
+                    }
+                }
+
+                List<IPAddress> listsorted = new List<IPAddress>(list);
+                listsorted.Sort(new IPAddressComparer());
+                for (int i = 0; i < listsorted.Count; i++)
+                {
+                    Console.WriteLine(listsorted[i].ToString());
+                    rs += listsorted[i].ToString() + Environment.NewLine;
+                }
+
+                html = html.Replace(" var IPaddress =\"",
+                    string.Format(" var IPaddress =\"{0}\";//", listsorted[0].ToString()));
+                rs += "Used RQA server for starting point: " + listsorted[0].ToString();
+                Console.WriteLine("Used RQA server for starting point: " + listsorted[0].ToString());                
+                File.WriteAllText(startingPage, html);
+            }
+            else
+            {
+                Console.WriteLine("Cannot found Starting point on this machine");
+                Console.WriteLine("File does not exist: " + startingPage);
+                rs += "Cannot found Starting point on this machine, File does not exist: " + startingPage;
+            }
+            return rs;
+        }
     }
+    /// <summary>
+    /// hthngoc - Sorting IP address
+    /// </summary>
+    public class IPAddressComparer : IComparer<IPAddress>
+    {
+        public int Compare(IPAddress x, IPAddress y)
+        {
+            int retVal = 0;
+            byte[] bytesX = x.GetAddressBytes();
+            byte[] bytesY = y.GetAddressBytes();
+
+            for (int i = 0; (i < bytesX.Length) && (i < bytesY.Length); i++)
+            {
+                retVal = bytesX[i].CompareTo(bytesY[i]);
+                if (0 != retVal)
+                {
+                    break;
+                }
+            }
+            return retVal;
+        }
+    }
+    
 }
